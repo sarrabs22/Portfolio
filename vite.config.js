85cc8e52f0d211437820ae2ad94752b91b4ebfd9
sarrabs22 <sarra.bensedrine@esprit.tn>
2026-05-05 14:26:28 +0100

    fix deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: './',
   plugins: [react()],
+  build: {
+    outDir: 'docs'
+  },
   server: {
     port: 5173,
     open: true
